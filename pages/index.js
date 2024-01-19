16119935bf21b0659f34aa5524339bc1b67183e1
rahim-khan-iitg <k.rahim@iitg.ac.in>
2024-01-19 23:47:16 +0530

    some changes

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">do a post request to the endpoint /api/convertBylink <br/>
-        and the json payload should be of the type &#123; "url":"url containing the audio file" &#125;
+        and the json payload should be of the type &#123; &quot;url&quot;:&quot;url containing the audio file&quot; &#125;
     </div>
   );
 }
